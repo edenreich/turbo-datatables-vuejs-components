23607e52aaf7537321a4d6f7460812127aa30b23
Eden Reich <eden.reich@gmail.com>
2018-10-23 23:50:52 +0200

    add UserRepository

---
+++ examples/php/symfony/src/Repository/UserRepository.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\User;
+use Doctrine\ORM\QueryBuilder;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * @method User|null find($id, $lockMode = null, $lockVersion = null)
+ * @method User|null findOneBy(array $criteria, array $orderBy = null)
+ * @method User[]    findAll()
+ * @method User[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class UserRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, User::class);
+    }
+
+    public function orderBy(string $column, string $direction): QueryBuilder
+    {
+        return $this->createQueryBuilder('users')->orderBy($column, $direction);
+    }
+}
